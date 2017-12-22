539e8490770463b3400085dbd94f7df6376acd1c
jonathanmourtada <jonathan.mourtada@divid.se>
2017-12-22 10:47:11 +0100

    day 15 part1

---
+++ 15/part1.ts
@@ -0,0 +1,33 @@
+function createGenerator(factor: number) {
+  return (prevValue: number) => {
+    return (prevValue * factor) % 2147483647;
+  };
+}
+
+// Example
+const genA = createGenerator(16807);
+const genB = createGenerator(48271);
+
+// Example
+// let genALastValue = 65;
+// let genBLastValue = 8921;
+
+// input
+let genALastValue = 618;
+let genBLastValue = 814;
+
+let pairs = 0;
+
+for (let i = 0; i < 40000000; i++) {
+  genALastValue = genA(genALastValue);
+  genBLastValue = genB(genBLastValue);
+
+  if (
+    genALastValue.toString(2).substr(-16) ===
+    genBLastValue.toString(2).substr(-16)
+  ) {
+    pairs++;
+  }
+}
+
+console.log(pairs);
